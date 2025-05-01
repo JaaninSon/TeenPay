fcc6658f60bf0b6ded9c4b60808fe16873ed070b
JaaninSon <jaan1ove@naver.com>
2025-05-01 16:21:41 +0900

    chore: downgrade TailwindCSS to v3 and update related config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        kakao: "#FEE500",
+      },
+    },
   },
   plugins: [],
 };
