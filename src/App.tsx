7183906a0e208368e650020b22e4def8049c63e5
JaaninSon <jaan1ove@naver.com>
2025-05-09 13:49:59 +0900

    chore: install react-toastify for toast notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import Router from "./routes/Router";
 import { AuthProvider } from "./contexts/AuthContext";
 import AppLayout from "./components/layouts/AppLayout";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <AuthProvider>
       <AppLayout>
         <Router />
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={true}
+          theme="colored"
+        />
       </AppLayout>
     </AuthProvider>
   );
