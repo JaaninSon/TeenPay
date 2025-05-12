3068029c710dbb349fb05739ee53d0efafb35282
JaaninSon <jaan1ove@naver.com>
2025-05-12 19:12:56 +0900

    feat: add reusable logout function with Firebase signOut

---
+++ src/utils/logout.ts
@@ -0,0 +1,17 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../services/firebase";
+import { NavigateFunction } from "react-router-dom";
+import { toast } from "react-toastify";
+
+export function handleLogout(navigate: NavigateFunction) {
+  signOut(auth)
+    .then(() => {
+      localStorage.clear();
+      navigate("/");
+      toast.success("로그아웃 되었습니다.");
+    })
+    .catch((err) => {
+      console.error("로그아웃 실패", err);
+      toast.error("로그아웃에 실패했습니다.");
+    });
+}
